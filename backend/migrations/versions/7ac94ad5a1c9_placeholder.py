58bbb3e860c656684c3e5512381e71227715892b
عمران <ouakouriimran@gmail.com>
2026-05-21 15:09:38 +0100

    fix: add stub migration for untracked revision 7ac94ad5a1c9

---
+++ backend/migrations/versions/7ac94ad5a1c9_placeholder.py
@@ -0,0 +1,22 @@
+"""placeholder for untracked migration
+
+Revision ID: 7ac94ad5a1c9
+Revises: 3fa23be2366b
+Create Date: 2026-05-21
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '7ac94ad5a1c9'
+down_revision = '3fa23be2366b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
